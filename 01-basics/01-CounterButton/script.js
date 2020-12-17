9039baa1ff6039025dd6f1367c1513f730d5aa46
Alyona Tyunyaeva <alyonatyunyaeva@gmail.com>
2020-12-17 14:54:37 +0300

    add solution for 01-basics/01-CounterButton

--- 01-basics/01-CounterButton/script.js
+++ 01-basics/01-CounterButton/script.js
@@ -1,4 +1,14 @@
 import Vue from './vue.esm.browser.js';
 
-// const app = ...
+const app = new Vue ({
+    el: '#app',
+    data: {
+        currentNum: 0,
+    },
+    methods: {
+        addPlusOne: function () {
+            return this.currentNum += 1;
+        },
+    }
+})
 // Рекомендуется использовать МЕТОД в качестве обработчика события

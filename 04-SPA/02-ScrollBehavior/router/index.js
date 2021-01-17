ac558868080198fbbe88518ce4fba78fa0d88189
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-01-17 23:26:00 +0300

    Added solution for task: ScrollBehavior

--- 04-SPA/02-ScrollBehavior/router/index.js
+++ 04-SPA/02-ScrollBehavior/router/index.js
@@ -3,14 +3,34 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-export function scrollBehavior() {
-  // Место для решения
+export function scrollBehavior(to, from, savedPosition) {
+  // Если есть hash, прокручиваем к элементу по селектору
+  if (to.hash) {
+    return { selector: to.hash };
+  }
+
+  // Если есть savedPosition, возвращаем его же (при переходе назад)
+  if (savedPosition) {
+    return savedPosition;
+  }
+
+  // Если оба маршрута в мета правилах имели saveScrollPosition, не меняем положение.
+  // Можно также использовать прямой вариант, если нужно точное совпадение, и дочерний маршрут может отменить правило
+  // if (to.meta.saveScrollPosition && from.meta.saveScrollPosition)
+  const hasSavePosition = (route) =>
+    route.matched.some((matched) => matched.meta.saveScrollPosition);
+  if (hasSavePosition(to) && hasSavePosition(from)) {
+    return false;
+  }
+
+  // По умолчанию прокручиваем вверх
+  return { x: 0, y: 0 };
 }
 
 export const router = new VueRouter({
   mode: 'history',
 
-  base: '/04-SPA/02-ScrollBehavior',
+  base: process.env.BASE_URL,
 
   scrollBehavior,
 
@@ -42,12 +62,18 @@ export const router = new VueRouter({
           alias: 'description',
           name: 'meetup-description',
           props: true,
+          meta: {
+            saveScrollPosition: true,
+          },
           component: () => import('../views/MeetupDescriptionPage'),
         },
         {
           path: 'agenda',
           name: 'meetup-agenda',
           props: true,
+          meta: {
+            saveScrollPosition: true,
+          },
           component: () => import('../views/MeetupAgendaPage'),
         },
       ],
